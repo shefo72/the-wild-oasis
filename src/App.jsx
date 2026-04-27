64550ba1a426696e9dee9843ab00b6765a4eb778
Ahmed Sherif <ahmedsherifeg22@gmail.com>
2026-04-27 16:53:00 +0300

    feat: finish UI styling with styled-components and integrate Supabase backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import GlobalStyle from "./styles/GlobalStyles";
-
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
-import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
-import Login from "./pages/Login";
+import Users from "./pages/Users";
 import Settings from "./pages/Settings";
-import NewUsers from "./pages/Users";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
+      <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="/dashboard" />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/bookings" element={<Bookings />} />
-            <Route path="/cabins" element={<Cabins />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/users" element={<NewUsers />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
           </Route>
-          <Route path="/login" element={<Login />} />
+
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
